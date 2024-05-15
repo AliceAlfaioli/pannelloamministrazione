3defaa8162e63a99bdb59c842a4a2f9f83af1639
AliceAlfaioli <156820194+AliceAlfaioli@users.noreply.github.com>
2024-05-15 16:45:07 +0200

    First commit

---
+++ admin.php
@@ -0,0 +1,36 @@
+<?php
+include __DIR__ . '/header.php';
+include __DIR__ . '/classes/Product.php';
+include_once __DIR__.'/getCard.php'; 
+
+    ?>
+
+
+
+    <h1 class="my-5">Sezione amministratore</h1>
+    
+
+<div class="row">
+
+<?php
+
+
+foreach ($articoli as $row) {
+    $product = new Product($row['id'], $row['nome'], $row['prezzo'], $row['img']);
+}
+
+?>
+
+
+
+
+</div>
+
+
+
+
+<?php
+
+include __DIR__ . '/footer.php';
+
+?>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+<nav class="navbar bg-body-tertiary">
+        <div class="container">
+            <span class="navbar-brand mb-0 h1">Alice's garden 🌸</span>
+
+            <div><a href="index.php">Esci</a>
+            <a href="inserisci.php">Inserisci</a></div>
+            
+        </div>
+
+    </nav>
+    
+    <div class="container py-5">
